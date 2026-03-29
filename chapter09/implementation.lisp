544ec802a0afcd363fcaec6d55fc53636499cb59
Vu Dieu Minh <vudieuminh1973@gmail.com>
2026-03-29 23:10:57 +0700

    Add examples for String and Charater primitives

--- chapter09/implementation.lisp
+++ chapter09/implementation.lisp
@@ -51,10 +51,13 @@
 ;;;; =========================================================================
 ;;;; EVAL
 ;;;;==========================================================================
-
+; Notes: EVAL takes a single argument and evaluate it.
 (defparameter form-to-evaluate '(+ 2 2))
 (print (eval form-to-evaluate))
 (print (read)) (+ 2 2)
+
+; READ returns a data structure like a list
+; which EVAL processes as code.
 (print (eval (read))) (+ 2 2)
 
 ;;;; =========================================================================
@@ -65,10 +68,32 @@
 (format t "~A~%" (length "abc"))
 (format t "~A~%" (reverse '(a b c)))
 (format t "~A~%" (reverse "a b c"))
+
 (format t "~A~%" (elt '(a b c) 0))
 (format t "~A~%" (elt '(a b c) 2))
 (format t "~A~%" (elt "abc" 0))
 (format t "~A~%" (elt "abc" 2))
+
 (format t "~A~%" (string= "abc" "xyz"))
 (format t "~A~%" (string= "abc" "abc"))
 (format t "~A~%" (string= "abc" "ABC"))
+
+(format t "~A~%" (char= #\a #\b))
+(format t "~A~%" (char= #\a #\a))
+(format t "~A~%" (char= #\a #\A))
+
+(format t "~A~%" (char-equal #\a #\b))
+(format t "~A~%" (char-equal #\a #\a))
+(format t "~A~%" (char-equal #\a #\A))
+
+(format t "~A~%" (search "Katz" "Katz, Boris"))
+(format t "~A~%" (search "Boris" "Katz, Boris"))
+(format t "~A~%" (search "Pushkin" "Katz, Boris"))
+(format t "~A~%" (search "BORIS" "Katz, Boris"))
+(format t "~A~%" (search "BORIS" "Katz, Boris" :test #'char-equal))
+(format t "~A~%" (search '(katz) '(katz boris)))
+(format t "~A~%" (search '(boris) '(katz boris)))
+(format t "~A~%" (search '(pushkin) '(katz boris)))
+(format t "~A~%" ((read-line)This is a READ-LINE test))
+(format t "~A~%" ((read-char)x))
+(format t "~A~%" ((read-char)X))
