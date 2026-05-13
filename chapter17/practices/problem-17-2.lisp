ee016daafbafc1d9bd2d4ba1785b8ffd060789ff
Vu Dieu Minh <vudieuminh1973@gmail.com>
2026-05-13 23:17:56 +0700

    Chapter 17: Solve problem 17.2

---
+++ chapter17/practices/problem-17-2.lisp
@@ -0,0 +1,21 @@
+;;;; ============================================================
+;;;; Chapter 17 - Problem 17-2
+;;;; Source: LISP - Patrick Henry Winston and Berthold Klaus Paul Horn
+;;;; ============================================================
+
+(defun user-defined-nreverse (lst)
+    (if (endp lst)
+        nil
+        (let ((prev nil) (curr lst) (next nil))
+            (loop
+                (when (null curr) ( return prev))
+
+                (setf next (cdr curr))       
+                (setf (cdr curr) prev)
+                
+                (setf prev curr)
+                (setf curr next)))
+        ))
+
+(defparameter *lst01* '(h e l l o))
+(format t "~A~%" (user-defined-nreverse *lst01*))
