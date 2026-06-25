1153b6bf8da374d6e114a0cc77f5b27eb2c109fe
Vu Dieu Minh <vudieuminh1973@gmail.com>
2026-06-25 22:16:28 +0700

    add examples for chapter 18

--- chapter18/implementation.lisp
+++ chapter18/implementation.lisp
@@ -4,183 +4,134 @@
 ; ;;; Author: Patrick Henry Winston and Berthold Klaus Paul Horn
 ; ;;; ============================================================
 
-;;;; ============================================================
-;;;; MICRO-EVAL and MICRO-APPLY
-;;;; ============================================================
-(defun micro-eval (form &optional environment)
-    (if (atom form)
-        (cond ((numberp form) form)
-            ((eq t form) t)
-            ((eq nil form) nil)
-            (t (let ((binding (assoc form environment)))
-                (if binding 
-                    (second binding)
-                    (error "I could find no variable binding for ~a." form)))))
-        (case (first form)
-            (m-quote (second form))
-            (m-if (if (micro-eval (second form) environment)
-                        (micro-eval (third form) environment)
-                        (micro-eval (fourth form) environment)))
-            (t (micro-apply
-                    (first form)
-                    (mapcar #'(lambda (x) (micro-eval x environment))
-                            (rest form)))))))
+;;; ------------------------------------------------------------
+;;; 1. Helper Functions
+;;; ------------------------------------------------------------
 
 (defun bind-variables (variables values &optional environment)
+  "Binds a list of variables to their corresponding values and prepends to the environment."
   (append (mapcar #'list variables values) environment))
 
+;;; ------------------------------------------------------------
+;;; 2. Meta-Circular Evaluator (MICRO-EVAL)
+;;; ------------------------------------------------------------
+
+(defun micro-eval (form &optional environment)
+  (if (atom form)
+      (cond ((numberp form) form)
+            ((eq t form) t)
+            ((eq nil form) nil)
+            (t (let ((binding (assoc form environment)))
+                 (if binding 
+                     (second binding)
+                     (error "I could find no variable binding for ~a." form)))))
+      (case (first form)
+        (m-quote (second form))
+        (m-if (if (micro-eval (second form) environment)
+                  (micro-eval (third form) environment)
+                  (micro-eval (fourth form) environment)))
+        (m-apply 
+         (micro-apply 
+          (micro-eval (second form) environment)
+          (micro-eval (third form) environment)))
+        (m-function 
+         ;; Captures the current lexical environment into a closure bundle
+         (list 'm-closure (second form) environment))
+        (t (micro-apply
+            (first form)
+            (mapcar #'(lambda (x) (micro-eval x environment))
+                    (rest form)))))))
+
+;;; ------------------------------------------------------------
+;;; 3. Meta-Circular Applicator (MICRO-APPLY)
+;;; ------------------------------------------------------------
+
 (defun micro-apply (procedure argument-values)
-    (if (symbolp procedure)
-        (case procedure
-            (m-first (first (first argument-values)))
-            (m-rest (rest (first argument-values)))
-            (m-cons (cons (first argument-values)
-                            (second argument-values)))
-            (m-endp (endp (first argument-values)))
-            (m-not (not (first argument-values)))
-            (m-eq (eq (first argument-values)
-                        (second argument-values)))
-            (t (let ((procedure-description (get procedure 'm-lambda)))
-                (if procedure-description
-                    (micro-apply procedure-description argument-values)
-                    (error "I could find no procedure description for ~a." procedure)))))
-        (micro-eval (third procedure)
-                    (bind-variables (second procedure) argument-values))))
+  (if (symbolp procedure)
+      (case procedure
+        (m-first (first (first argument-values)))
+        (m-rest (rest (first argument-values)))
+        (m-cons (cons (first argument-values)
+                      (second argument-values)))
+        (m-endp (endp (first argument-values)))
+        (m-not (not (first argument-values)))
+        (m-eq (eq (first argument-values)
+                  (second argument-values)))
+        (t (let ((procedure-description (get procedure 'm-lambda)))
+             (if procedure-description
+                 (micro-apply procedure-description argument-values)
+                 (error "I could find no procedure description for ~a." procedure)))))
+      (case (first procedure)
+        (m-lambda
+         (micro-eval (third procedure)
+                     (bind-variables (second procedure)
+                                     argument-values)))
+        (m-closure 
+         ;; Destructures: (m-closure (m-lambda (params) (body)) saved-env)
+         (let ((lambda-form (second procedure))
+               (closure-env (third procedure)))
+           (micro-eval (third lambda-form)
+                       (bind-variables (second lambda-form)
+                                       argument-values
+                                       closure-env))))
+        (t (error "Unknown procedure type: ~a" procedure)))))
 
-;(format t "~a~%"(error "There has been a fatal error."))
+;;; ------------------------------------------------------------
+;;; 4. Execution & Automated Test Cases
+;;; ------------------------------------------------------------
 
-;; Example
-(format t "~a~%" (micro-eval '(m-first (m-quote (a b c)))))
+;; Enable trace to monitor micro-eval and micro-apply call frames
 (trace micro-eval micro-apply)
-(format t "~a~%" (micro-eval '(m-first (m-quote (a b c)))))
 
-;; Example with lambda-style
-(setf (get 'm-second 'm-lambda)
-    '(m-lambda (l) (m-first (m-rest l))))
+(format t "=== Running Test Suite ===~%~%")
 
-(format t "~A~%" (micro-eval '(m-second (m-quote (a b c)))))
-(format t "~A~%" (micro-eval '((M-LAMBDA (L) (M-FIRST (M-REST L)))
-                                (M-QUOTE (A B C)))))
-(format t "~A~%" (micro-eval '(m-second (m-quote (a b c)))))
+;; Test 1: Basic Evaluation
+(format t "--- Test 1: Basic Primitive Evaluation ---~%")
+(format t "Output: ~A~%~%" (micro-eval '(m-first (m-quote (a b c)))))
 
+;; Test 2: Symbol-Bound Lambda (via property list)
+(format t "--- Test 2: Symbol-Bound Lambda Property ---~%")
+(setf (get 'm-second 'm-lambda) '(m-lambda (l) (m-first (m-rest l))))
+(format t "Output: ~A~%~%" (micro-eval '(m-second (m-quote (a b c)))))
+
+;; Test 3: Inline Lambda Execution
+(format t "--- Test 3: Direct Compound Lambda Execution ---~%")
+(format t "Output: ~A~%~%" (micro-eval '((m-lambda (l) (m-first (m-rest l)))
+                                         (m-quote (a b c)))))
+
+;; Test 4: Recursive Execution (m-append)
+(format t "--- Test 4: Recursive Function Definition ---~%")
 (setf (get 'm-append 'm-lambda)
-        '(m-lambda (l1 l2)
-            (m-if (m-endp l1)
-                l2
-                (m-cons (m-first l1)
-                        (m-append (m-rest l1) l2)))))
+      '(m-lambda (l1 l2)
+         (m-if (m-endp l1)
+               l2
+               (m-cons (m-first l1)
+                       (m-append (m-rest l1) l2)))))
+(format t "Output: ~A~%~%" (micro-eval '(m-append (m-quote (a b c)) (m-quote (x y z)))))
 
-(format t "~A~%" (micro-eval '(m-append (m-quote (a b c)) (m-quote (x y z)))))
+;; Test 5: Explicit m-apply Evaluation
+(format t "--- Test 5: Meta-Apply of Explicit Lambda ---~%")
+(format t "Output: ~A~%~%" (micro-eval '(m-apply 
+                                         (m-quote (m-lambda (x y) (m-cons x (m-cons y nil))))
+                                         (m-quote (a b)))))
 
-;; Evaluate Form 
-(defun micro-eval (form &optional environment)
-    (if (atom form)
-        (cond ((numberp form) form)
-            ((eq t form) t)
-            ((eq nil form) nil)
-            (t (let ((binding (assoc form environment)))
-                (if binding 
-                    (second binding)
-                    (error "I could find no variable binding for ~a." form)))))
-        (case (first form)
-            (m-quote (second form))
-            (m-if (if (micro-eval (second form) environment)
-                        (micro-eval (third form) environment)
-                        (micro-eval (fourth form) environment)))
-            (m-apply 
-                (micro-apply 
-                    (micro-eval (second form) environment)
-                    (micro-eval (third form) environment)))
-            (t (micro-apply
-                    (first form)
-                    (mapcar #'(lambda (x) (micro-eval x environment))
-                            (rest form)))))))
+;; Test 6: Nested Functional Application (Dynamic Scoping)
+(format t "--- Test 6: Nested Functional Scoping ---~%")
+(format t "Output: ~A~%~%" (micro-eval '(m-apply 
+                                         (m-quote
+                                          (m-lambda (x y)
+                                            (m-apply 
+                                             (m-quote (m-lambda (l m) (m-cons l (m-cons m nil))))
+                                             (m-cons x (m-cons y nil)))))
+                                         (m-quote (a b)))))
 
-(format t "~A~%" (micro-eval '(m-apply 
-                                (m-quote (m-lambda (x y)
-                                                    (m-cons x (m-cons y nil))))
-                                (m-quote (a b)))))
-
-(micro-eval '(m-apply 
-                (m-quote
-                    (m-lambda (x y)
-                        (m-apply 
-                            (m-quote
-                                (m-lambda (l m)
-                                    (m-cons l (m-cons m nil))))
-                            (m-cons x (m-cons y nil)))))
-                (m-quote (a b))))
-
-(micro-eval '(m-apply 
-                (m-quote 
-                    (m-lambda (x y)
-                        (m-apply
-                            (m-quote
-                                (m-lambda ()
-                                    (m-cons x (m-cons y nil))))
-                                (m-quote ()))))
-                (m-quote (a b))))
-
-(defun micro-apply (procedure argument-values)
-    (if (symbolp procedure)
-        (case procedure
-            (m-first (first (first argument-values)))
-            (m-rest (rest (first argument-values)))
-            (m-cons (cons (first argument-values) 
-                            (second argument-values)))
-            (m-endp (endp (first argument-values)))
-            (m-not (not (first argument-values)))
-            (m-eq (eq (first argument-values)
-                    (second argument-values)))
-            (t (let ((procedure-description (get procedure 'm-lambda)))
-                    (if procedure-description
-                        (micro-apply procedure-description argument-values)
-                        (error 
-                            "I could find no procedure description for ~a." 
-                            procedure)))))
-        (case (first procedure)
-            (m-lambda
-                (micro-eval (third procedure)
-                            (bind-variables (second procedure)
-                                            argument-values)))
-            (m-closure
-                (micro-eval (third procedure)
-                            (bind-variables (second procedure)
-                                            argument-values
-                                            (fourth procedure)))))))
-
-(format t "~A~%" (micro-eval '(m-first (m-quote (a b c)))))
-(defun micro-eval (form &optional environment)
-    (if (atom form)
-        (cond ((numberp form) form)
-            ((eq t form) t)
-            ((eq nil form) nil)
-            (t (let ((binding (assoc form environment)))
-                (if binding 
-                    (second binding)
-                    (error "I could find no variable binding for ~a." form)))))
-        (case (first form)
-            (m-quote (second form))
-            (m-if (if (micro-eval (second form) environment)
-                        (micro-eval (third form) environment)
-                        (micro-eval (fourth form) environment)))
-            (m-apply 
-                (micro-apply 
-                    (micro-eval (second form) environment)
-                    (micro-eval (third form) environment)))
-            (m-function ;; <-- THÊM NHÁNH NÀY ĐỂ TẠO CLOSURE
-                (list 'm-closure (second form) environment))
-            (t (micro-apply
-                    (first form)
-                    (mapcar #'(lambda (x) (micro-eval x environment))
-                            (rest form)))))))
-                            
-(micro-eval '(m-apply 
-                (m-function
-                    (m-lambda (x y)
-                        (m-apply
-                            (m-function 
-                                (m-lambda ()
-                                    (m-cons x (m-cons y nil))))
-                        (m-quote ()))))
-                (m-quote (a b))))
+;; Test 7: Lexical Scoping with Closures (The failing test case!)
+(format t "--- Test 7: Lexical Closure Isolation (m-function) ---~%")
+(format t "Output: ~A~%~%" (micro-eval '(m-apply 
+                                         (m-function
+                                          (m-lambda (x y)
+                                            (m-apply
+                                             (m-function 
+                                              (m-lambda () (m-cons x (m-cons y nil))))
+                                             (m-quote ()))))
+                                         (m-quote (a b)))))
