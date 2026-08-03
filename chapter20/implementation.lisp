8907e8bd9ffa19c8cc05741c4bf6c450702dab4d
Vu Dieu Minh <vudieuminh1973@gmail.com>
2026-08-03 15:25:09 +0700

    Fix circular printing and implement event queue for simulation
    - Added custom print functions to prevent stack overflow (circular reference).
    - Implemented priority queue (*event-sequence*) to run tasks in correct chronological order.
    - Fixed syntax errors in defstruct and format directives.

--- chapter20/implementation.lisp
+++ chapter20/implementation.lisp
@@ -4,12 +4,26 @@
 ; ;;; Author: Patrick Henry Winston and Berthold Klaus Paul Horn
 ; ;;; ============================================================
 
-(defstruct event
+(defvar *start* nil)
+(defvar *event-sequence* nil)
+
+(defun print-event (structure &rest ignore)
+    (format t "<event structure>"))
+
+(defun print-task (structure &rest ignore)
+  (format t "<task structure for ~a>" (task-name structure)))
+
+; (defstruct event
+;     (time 'unknown)
+;     (input-task nil)
+;     (output-tasks nil))
+
+(defstruct (event (:print-function print-event))
     (time 'unknown)
     (input-task nil)
     (output-tasks nil))
 
-(defstruct task
+(defstruct (task (:print-function print-task))
     (name 'unknown)
     (input-events nil)
     (output-event nil)
@@ -21,21 +35,6 @@
         (push task (event-output-tasks event)))
     (setf (task-output-event task) output)
     (setf (event-input-task output) task))
-
-(let ((s (make-task :duration 4 :name 'select))
-    (n (make-task :duration 6 :name 'negotiate))
-    (d (make-task :duration 4 :name 'design))
-    (p (make-task :duration 7 :name 'prepare))
-    (f (make-task :duration 8 :name 'furnish))
-    (m (make-task :duration 1 :name 'move))
-    (e1 (make-event :time 0))
-    (e2 (make-event)) (e3 (make-event))
-    (e4 (make-event)) (e5 (make-event))
-    (e6 (make-event)) (e7 (make-event)))
-    (setf *start* e1)
-    (connect s e2 (list e1)) (connect n e3 (list e2))
-    (connect d e4 (list e2)) (connect p e5 (list e3 e4))
-    (connect f e6 (list e3 e4)) (connect m e7 (list e5 e6)) 'done)
 
 (defun simulate-event (time event)
     (setf (event-time event) time)
@@ -49,3 +48,69 @@
 
 (defun latest-time (events)
     (apply #'max (mapcar #'event-time events)))
+
+; (defun simulate-task (task)
+;     (when (event-times-known-p (task-input-events task))
+;         (let* ((start-time (latest-time (task-input-events task)))
+;                 (finish-time (+ (task-duration task) start-time)))
+;             (announce-start-time start-time (task-name task))
+;             (announce-finish-time finish-time (task-name task))
+;             (simulate-event finish-time (task-output-event task)))))
+
+(defun simulate-task (task)
+  (when (event-times-known-p (task-input-events task))
+    (let* ((start-time (latest-time (task-input-events task)))
+           (finish-time (+ (task-duration task) start-time)))
+      (add-to-event-sequence 
+       `(announce-start-time ,start-time ',(task-name task)))
+      (add-to-event-sequence 
+       `(announce-finish-time ,finish-time ',(task-name task)))
+      (add-to-event-sequence 
+       `(simulate-event ,finish-time ,(task-output-event task))))))
+
+(defun simulate (starting-event time)
+    (setf *event-sequence* nil) ;; clear old event.
+    (simulate-event time starting-event)
+    (loop 
+        (if (endp *event-sequence*) ;; if don't have event then done
+            (return 'done)
+            (eval (pop *event-sequence*)))))
+
+(defun announce-start-time (time name)
+    (format t "~%Time ~a: ~9tstarting~19t~a." time name))
+
+(defun announce-finish-time (time name)
+    (format t "~%Time ~a: ~9tfinishing~19t~a." time name))
+
+(defun earlier-first-p (x y)
+  (cond ((< (second x) (second y)) t)
+        ((= (second x) (second y))
+         (cond ((eq 'announce-finish-time (first x)) t)
+               ((eq 'announce-finish-time (first y)) nil)
+               ((eq 'simulate-event (first x)) t)
+               ((eq 'simulate-event (first y)) nil)))))
+
+(defun add-to-event-sequence (form)
+  (setf *event-sequence*
+        (sort (cons form *event-sequence*)
+              #'earlier-first-p)))
+
+(let ((s (make-task :duration 4 :name 'select))
+    (n (make-task :duration 6 :name 'negotiate))
+    (d (make-task :duration 4 :name 'design))
+    (p (make-task :duration 7 :name 'prepare))
+    (f (make-task :duration 8 :name 'furnish))
+    (m (make-task :duration 1 :name 'move))
+    (e1 (make-event :time 0))
+    (e2 (make-event)) (e3 (make-event))
+    (e4 (make-event)) (e5 (make-event))
+    (e6 (make-event)) (e7 (make-event)))
+    (setf *start* e1)
+    (connect s e2 (list e1)) 
+    (connect n e3 (list e2))
+    (connect d e4 (list e2)) 
+    (connect p e5 (list e3 e4))
+    (connect f e6 (list e3 e4)) 
+    (connect m e7 (list e5 e6)) 'done)
+
+(format t "~A~%" (simulate *start* 0))
